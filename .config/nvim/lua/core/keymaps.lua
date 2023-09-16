0fbc4727a2d5001dc113688f66059d2cd7171e53
johanyim <37012949+johanyim@users.noreply.github.com>
2023-09-16 15:06:06 +0100

    removed unnecessary files, copy to system clipboard working

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -10,12 +10,17 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 
--- use spaces for tabs and whatnot
-
+-- use spaces for tab
 local TAB_SIZE = 4
 vim.opt.tabstop = TAB_SIZE
 vim.opt.shiftwidth = TAB_SIZE
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- system clipboard yanking with control + c
+-- control + p works as well
+vim.keymap.set('n','<leader>h',     ':nohlsearch<CR>')
+vim.keymap.set('n','<C-c>',         '"*y :let @+=@*<CR>',{noremap=true})
+vim.keymap.set('v','<C-c>',         '"*y :let @+=@*<CR>',{noremap=true})
+vim.keymap.set('n','<C-v>',         '"+P',{noremap=true})
+vim.keymap.set('v','<C-v>',         '"+P',{noremap=true})
