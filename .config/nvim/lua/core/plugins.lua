a454bdeaba2097abad96f7a83db52dcda6b4b340
johanyim <37012949+johanyim@users.noreply.github.com>
2023-10-16 13:02:33 +0100

    switching to main branch dwm

---
+++ .config/nvim/lua/core/plugins.lua
@@ -0,0 +1,72 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use 'nvim-tree/nvim-tree.lua'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'nvim-lualine/lualine.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    use {
+        'nvim-telescope/telescope.nvim',
+
+        tag = '0.1.2',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'neovim/nvim-lspconfig',
+    }
+    --completions for lua
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use({
+	    "L3MON4D3/LuaSnip",
+	    tag = "v2.*",
+	    run = "make install_jsregexp"
+    })
+
+    --completions for all languages
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'rafamadriz/friendly-snippets'
+
+    --rust tools
+    use 'simrat39/rust-tools.nvim'
+
+    --rgba colors 
+    use 'norcalli/nvim-colorizer.lua'
+
+    --markdown previews in browser
+    use({ 
+        "iamcco/markdown-preview.nvim", 
+        run = "cd app && npm install", 
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
+        ft = { "markdown" }, 
+    })
+    
+    use({
+        "aurum77/live-server.nvim",
+        run = function()
+            require"live_server.util".install()
+        end,
+        cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+    })
+
+
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
