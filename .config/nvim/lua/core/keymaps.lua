ff82892125724149f87e707cd3183fe12e2f45d1
johanyim <37012949+johanyim@users.noreply.github.com>
2023-11-03 01:01:06 +0000

    alacritty recognizes ctrl+BS and nvim has ctrl keybinds

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
-vim.g.mapleader = ','
+vim.g.mapleader = ',' 
 vim.g.maplocalleader = ','
 
 vim.opt.showcmd = true
@@ -25,3 +25,11 @@ vim.opt.scrolloff= SCROLL_OFF
 vim.keymap.set('n','<leader>h',     ':nohlsearch<CR>')
 vim.keymap.set('n','<C-y>',         '"*y :let @+=@*<CR>',{noremap=true})
 vim.keymap.set('v','<C-y>',         '"*y :let @+=@*<CR>',{noremap=true})
+
+-- moving words with control and arrow like I'm used to
+vim.keymap.set('i','<C-Right>',         '<esc>ea',{noremap=true})
+vim.keymap.set('n','<C-Right>',         'e',{noremap=true})
+vim.keymap.set('v','<C-Right>',         'e',{noremap=true})
+vim.keymap.set('i','<C-Left>',          '<esc>bi',{noremap=true})
+vim.keymap.set('n','<C-Left>',          'b',{noremap=true})
+
