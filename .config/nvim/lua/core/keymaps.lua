39ef0cefc1ea002211d70940ac5c75f5d46310d7
johanyim <37012949+johanyim@users.noreply.github.com>
2023-11-03 01:31:42 +0000

    removed the backward-word mapping since it was already built in

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -27,9 +27,10 @@ vim.keymap.set('n','<C-y>',         '"*y :let @+=@*<CR>',{noremap=true})
 vim.keymap.set('v','<C-y>',         '"*y :let @+=@*<CR>',{noremap=true})
 
 -- moving words with control and arrow like I'm used to
-vim.keymap.set('i','<C-Right>',         '<esc>ea',{noremap=true})
 vim.keymap.set('n','<C-Right>',         'e',{noremap=true})
 vim.keymap.set('v','<C-Right>',         'e',{noremap=true})
-vim.keymap.set('i','<C-Left>',          '<esc>bi',{noremap=true})
 vim.keymap.set('n','<C-Left>',          'b',{noremap=true})
+vim.keymap.set('v','<C-Left>',          'b',{noremap=true})
+-- vim.keymap.set('i','<C-Left>',          'b',{noremap=true})
+vim.keymap.set('i','<C-Right>',          '<esc>ea',{noremap=true})
 
